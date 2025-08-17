7501929f2aa240ff83ec64dd25261f2072c4d8dc
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-08-17 18:16:01 +0300

    completed week 8 tasks

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",     
+        user="root",          
+        password="Mysql1!!Pass"
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print("Error while connecting to MySQL:", e)
+
+finally:
+    if 'mycursor' in locals() and mycursor:
+        mycursor.close()
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
